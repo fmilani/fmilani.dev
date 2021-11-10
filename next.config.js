85b48616bd388aae737c072fde675259dfe5b96f
Felipe Milani <felmilani@gmail.com>
2021-11-10 15:13:30 -0300

    Add flickr integration on photos page

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+})
+module.exports = {
+  ...withNextra(),
+  images: {
+    domains: ['live.staticflickr.com']
+  }
+}
